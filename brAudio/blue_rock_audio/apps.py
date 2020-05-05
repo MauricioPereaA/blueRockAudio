17496da44b7c762ebb23e8e597a4e869001f0f42
raccon <raccon@localhost.localdomain>
2020-05-05 14:54:19 -0500

    Start project-Django-App-SASS and boostrap in webpack

---
+++ brAudio/blue_rock_audio/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BlueRockAudioConfig(AppConfig):
+    name = 'blue_rock_audio'
---
+++ fx-braudio/config.js
@@ -0,0 +1,27 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    mode: 'development',
+    output: {
+        filename: 'index.js',
+        path: __dirname + '/../brAudio/static/js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]  
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/index.css',
+        }),
+    ]
+};
